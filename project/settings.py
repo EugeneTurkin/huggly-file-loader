9bc13e396e37d67204ea7ea8889c61eec99fab61
Eugene Turkin <eugeneturkin@gmail.com>
2025-05-23 16:49:35 +0300

    added huey

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.djhuey',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Huey task queue
+HUEY = {}
 
 # Custom Autoloader settings
 DOWNLOADS_PATH = BASE_DIR / "autoloader" / "downloads"
